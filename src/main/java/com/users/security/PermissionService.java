628332f090aa35d3dcfcfec7804da651f3c4ca1a
Paul O'Connor <jamespauloconnor@gmail.com>
2016-11-06 22:00:30 -0500

    Added the ability for new users to register and log in.

--- src/main/java/com/users/security/PermissionService.java
+++ src/main/java/com/users/security/PermissionService.java
@@ -4,11 +4,15 @@ import static com.users.security.Role.ROLE_ADMIN;
 import static com.users.security.Role.ROLE_USER;
 import static org.springframework.security.core.context.SecurityContextHolder.getContext;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import com.users.beans.User;
 import com.users.repositories.ContactRepository;
 import com.users.repositories.UserRepository;
 
@@ -21,13 +25,16 @@ public class PermissionService {
 	@Autowired
 	private ContactRepository contactRepository;
 
-	private UsernamePasswordAuthenticationToken getToken() {
-		return (UsernamePasswordAuthenticationToken) getContext().getAuthentication();
+	private AbstractAuthenticationToken getToken() {
+		return (AbstractAuthenticationToken) getContext().getAuthentication();
 	}
 
+
 	public long findCurrentUserId() {
-		return userRepository.findByEmail(getToken().getName()).get(0).getId();
+		List<User> users = userRepository.findByEmail(getToken().getName());
+		return users != null && !users.isEmpty() ? users.get(0).getId() : -1;
 	}
+
 
 	public boolean hasRole(Role role) {
 		for (GrantedAuthority ga : getToken().getAuthorities()) {
